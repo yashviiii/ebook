106ee3203411569fb86ccc0c766bd4f1a1fba1d7
yashviiii <yashvi.dhar@gmail.com>
2022-07-16 14:27:57 +0530

    Code Added

--- lib/home.dart
+++ lib/home.dart
@@ -19,92 +19,115 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         titleTextStyle: const TextStyle(color: Colors.white),
       ),
-      body: GridView.count(
-          padding: EdgeInsets.fromLTRB(25, 15, 25, 10),
-          crossAxisCount: 2,
-          mainAxisSpacing: 8,
-          crossAxisSpacing: 25,
-          scrollDirection: Axis.vertical,
-          childAspectRatio: 2 / 2.6,
-          physics: BouncingScrollPhysics(),
-          shrinkWrap: true,
-          children: [
-            GridCard(
-                onPress: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const WebViewExample(
-                              title: 'Library Genesis',
-                              url: 'https://libgen.is')));
-                },
-                text: 'Library Genesis',
-                image: 'assets/accounting-book.png'),
-            GridCard(
-                onPress: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const WebViewExample(
-                              title: 'LibriVox Audio Books',
-                              url: 'https://librivox.org')));
-                  // _launchInWebViewOrVC(Uri.parse('https://librivox.org'));
-                },
-                text: 'LibriVox Audio Books',
-                image: 'assets/audio-book (1).png'),
-            GridCard(
-                onPress: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const WebViewExample(
-                              title: 'Open Library Internet Archive', url: 'https://openlibrary.org')));
-                },
-                text: 'Open Library Internet Archive',
-                image: 'assets/books (1).png'),
-            GridCard(
-                onPress: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const WebViewExample(
-                              title: 'PDF Drives',
-                              url: 'https://www.pdfdrive.com')));
-                },
-                text: 'PDF Drives',
-                image: 'assets/book-shelf.png'),
-            GridCard(
-                onPress: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const WebViewExample(
-                              title: "Epic Reads",
-                              url: 'https://www.epicreads.com')));
-                },
-                text: 'Epic Reads',
-                image: 'assets/knowledge.png'),
-            GridCard(
-                onPress: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => WebViewExample(
-                              title: "Tantor Media", url: 'https://tantor.com')));
-                },
-                text: 'Tantor Media',
-                image: 'assets/audio-book.png'),
+      body:
+      // SingleChildScrollView(
+      //   child: Column(
+      //     children: [
+      //       SizedBox(height: 20,),
+      //       Text(
+      //           "PDF's",
+      //         style: TextStyle(
+      //           backgroundColor: Colors.grey,
+      //           fontSize: 22,
+      //           fontWeight: FontWeight.bold
+      //         ),
+      //       ),
+            GridView.count(
+                padding: EdgeInsets.fromLTRB(25, 15, 25, 10),
+                crossAxisCount: 2,
+                mainAxisSpacing: 8,
+                crossAxisSpacing: 25,
+                scrollDirection: Axis.vertical,
+                childAspectRatio: 2 / 2.6,
+                physics: BouncingScrollPhysics(),
+                shrinkWrap: true,
+                children: [
                   GridCard(
-                onPress: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => WebViewExample(
-                              title: "Z Library", url: 'https://1lib.in/')));
-                },
-                text: 'Z Library',
-                image: 'assets/books.png'),
-          ]),
+                      onPress: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => WebViewExample(
+                                    title: "Z Library", url: 'https://1lib.in/')));
+                      },
+                      text: 'Z Library',
+                      image: 'assets/books.jpg',
+                      color1: Colors.blue,
+                      color2: Colors.white,
+                  ),
+                  GridCard(
+                      onPress: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const WebViewExample(
+                                    title: 'LibriVox Audio Books',
+                                    url: 'https://librivox.org')));
+                        // _launchInWebViewOrVC(Uri.parse('https://librivox.org'));
+                      },
+                      text: 'LibriVox Audio Books',
+                      image: 'assets/audio-book (1).png',
+                      color1: Colors.green,
+                      color2: Colors.white,
+                  ),
+                  GridCard(
+                      onPress: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const WebViewExample(
+                                    title: 'Open Library Internet Archive', url: 'https://openlibrary.org')));
+                      },
+                      text: 'Open Library Internet Archive',
+                      image: 'assets/books (1).png', color1: Colors.purple, color2: Colors.white,),
+                  GridCard(
+                      onPress: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => WebViewExample(
+                                    title: "Tantor Media", url: 'https://tantor.com')));
+                      },
+                      text: 'Tantor Media Audio Books',
+                      image: 'assets/mermaid.jpg',color1: Colors.purple, color2: Colors.white),
+                  GridCard(
+                      onPress: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const WebViewExample(
+                                    title: "Epic Reads",
+                                    url: 'https://www.epicreads.com')));
+                      },
+                      text: 'Epic Reads',
+                      image: 'assets/knowledge.png', color1: Colors.redAccent, color2: Colors.white),
+
+                  GridCard(
+                      onPress: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const WebViewExample(
+                                    title: 'PDF Drives',
+                                    url: 'https://www.pdfdrive.com')));
+                      },
+                      text: 'PDF Drives',
+                      image: 'assets/book-shelf.png', color1: Colors.indigo, color2: Colors.white),
+                  GridCard(
+                      onPress: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const WebViewExample(
+                                    title: 'Library Genesis',
+                                    url: 'https://libgen.is')));
+                      },
+                      text: 'Library Genesis',
+                      image: 'assets/accounting-book.png',color1: Colors.purple, color2: Colors.white),
+                ]),
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
--- lib/widget/gridcard.dart
+++ lib/widget/gridcard.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class GridCard extends StatelessWidget {
+  final Color color1;
+  final Color color2;
   final String? image;
   final Function onPress;
   final String? text;
@@ -8,7 +10,7 @@ class GridCard extends StatelessWidget {
     Key? key,
     this.image,
     required this.onPress,
-    this.text,
+    this.text, required this.color1, required this.color2,
   }) : super(key: key);
 
   @override
@@ -17,34 +19,46 @@ class GridCard extends StatelessWidget {
       onTap: () {
         onPress();
       },
-      child: Card(
-          child: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.all(30),
-            child: Image.asset(image.toString()),
-            ///////////////UNCOMMENT IF YOU NEED NETWORK IMAGE
-            // child: CachedNetworkImage(
-            //   progressIndicatorBuilder: (context, url, progress) => Center(
-            //     child: CircularProgressIndicator(
-            //       value: progress.progress,
-            //     ),
-            //   ),
-            //   errorWidget: (context, url, error) => const Icon(Icons.error),
-            //   imageUrl: image.toString(),
-            // ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 2),
-            child: Text(
-              text.toString(),
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontFamily: 'Montserrat', fontWeight: FontWeight.w900),
+      child: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                color1,
+                Colors.orangeAccent,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            )
+        ),
+        child: Card(
+            child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Image.asset(image.toString()),
+              ///////////////UNCOMMENT IF YOU NEED NETWORK IMAGE
+              // child: CachedNetworkImage(
+              //   progressIndicatorBuilder: (context, url, progress) => Center(
+              //     child: CircularProgressIndicator(
+              //       value: progress.progress,
+              //     ),
+              //   ),
+              //   errorWidget: (context, url, error) => const Icon(Icons.error),
+              //   imageUrl: image.toString(),
+              // ),
             ),
-          )
-        ],
-      )),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 2),
+              child: Text(
+                text.toString(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontFamily: 'Montserrat', fontWeight: FontWeight.w900),
+              ),
+            )
+          ],
+        )),
+      ),
     );
   }
 }
